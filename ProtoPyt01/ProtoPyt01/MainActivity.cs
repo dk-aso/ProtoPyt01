68eca7bc058556d1a16d0f5c00658d74793e9354
allan <allan8722@gmail.com>
2016-10-31 21:07:12 +0100

    TxtColor & Pyt txt

--- ProtoPyt01/ProtoPyt01/MainActivity.cs
+++ ProtoPyt01/ProtoPyt01/MainActivity.cs
@@ -19,6 +19,11 @@ namespace ProtoPyt01
             TextView txtView = FindViewById<TextView>(Resource.Id.TextView1);
             string txt1 = "Så PYT da, og nu er det videre i livet";
             string txt2 = "Så er der PYTTET, og der er fri bane frem";
+            String txt3 = "Du har valgt at sige pyt, så kom videre i livet!";
+            String txt4 = "Da har valg at trykke PYT, det betyder at du ikke skal bruge mere energi på det du har pyttet!";
+            String txt5 = "PYT!! Og så er der fri bane fremad!";
+            String txt6 = "PYT!! Ting sker, og det der er sket lige nu er kun et PYT værd, og skal ikke have inflydelse på dit humør.";
+            String txt7 = "Det var så det! Med et PYT er det overstået og får ikke mere indflydelse på dig og dit liv.";
 
             button.Click += delegate
             {
