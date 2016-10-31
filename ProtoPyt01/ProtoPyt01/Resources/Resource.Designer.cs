24e17de433aeddbb8ae30f88f01f05125b582a19
alla3118 <alla3118@hedskole.dk>
2016-10-31 08:04:59 +0100

    graphigs

--- ProtoPyt01/ProtoPyt01/Resources/Resource.Designer.cs
+++ ProtoPyt01/ProtoPyt01/Resources/Resource.Designer.cs
@@ -45,7 +45,13 @@ namespace ProtoPyt01
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int GiftIcon72 = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int Icon = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Pyt1Button = 2130837506;
 			
 			static Drawable()
 			{
