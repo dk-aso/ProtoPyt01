e864662f5117f6eb23db8af6b18486621e0b4162
alla3118 <alla3118@hedskole.dk>
2016-11-02 09:23:51 +0100

    To fit Nexus 6P

--- ProtoPyt01/ProtoPyt01/Resources/Resource.Designer.cs
+++ ProtoPyt01/ProtoPyt01/Resources/Resource.Designer.cs
@@ -56,6 +56,9 @@ namespace ProtoPyt01
 			// aapt resource value: 0x7f020003
 			public const int WhiteRosePLastic = 2130837507;
 			
+			// aapt resource value: 0x7f020004
+			public const int WhiteRosePLastic1 = 2130837508;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
