8a96d3502758f81bbdcb6a4c89ca845e66e4e8ff
alla3118 <alla3118@hedskole.dk>
2016-11-16 14:38:47 +0100

    Graphics_background_fix

--- ProtoPyt01/ProtoPyt01/MainActivity.cs
+++ ProtoPyt01/ProtoPyt01/MainActivity.cs
@@ -20,8 +20,8 @@ namespace ProtoPyt01
             TextView txtView = FindViewById<TextView>(Resource.Id.TextView1);
             string txt1 = "Så PYT da, og nu er det videre med livet";
             string txt2 = "Så er der PYTTET, og der er fri bane frem";
-            string txt3 = "Du har valgt at sige pyt, så kom videre i livet!";
-            string txt4 = "Da har valg at trykke PYT, det betyder at du ikke skal bruge mere energi på det du har pyttet!";
+            string txt3 = "Du har valgt at sige PYT, så kom videre i livet!";
+            string txt4 = "Du har valg at trykke PYT, det betyder at du ikke skal bruge mere energi på det du netop har pyttet!";
             string txt5 = "PYT!! Og så er der fri bane fremad!";
             string txt6 = "PYT!! Ting sker, og det der er sket lige nu er kun et PYT værd, og skal ikke have inflydelse på dit humør.";
             string txt7 = "Det var så det! Med et PYT er det overstået og får ikke mere indflydelse på dig og dit liv.";
@@ -33,31 +33,38 @@ namespace ProtoPyt01
                 {
                     case 1:
                         txtView.Text = txt2;
-                        txtView.SetBackgroundColor(Color.ParseColor("#000000"));
+                        //txtView.SetBackgroundColor(Color.ParseColor("#000000"));
+                        txtView.SetBackgroundResource(Resource.Drawable.MyTextBackground2);
                         break;
                     case 2:
                         txtView.Text = txt3;
-                        txtView.SetBackgroundColor(Color.ParseColor("#545454"));
+                        //txtView.SetBackgroundColor(Color.ParseColor("#545454"));
+                        txtView.SetBackgroundResource(Resource.Drawable.MyTextBackground3);
                         break;
                     case 3:
                         txtView.Text = txt4;
-                        txtView.SetBackgroundColor(Color.ParseColor("#896262"));
+                        //txtView.SetBackgroundColor(Color.ParseColor("#896262"));
+                        txtView.SetBackgroundResource(Resource.Drawable.MyTextBackground4);
                         break;
                     case 4:
                         txtView.Text = txt5;
-                        txtView.SetBackgroundColor(Color.ParseColor("#ba9580"));
+                        //txtView.SetBackgroundColor(Color.ParseColor("#ba9580"));
+                        txtView.SetBackgroundResource(Resource.Drawable.MyTextBackground5);
                         break;
                     case 5:
                         txtView.Text = txt6;
-                        txtView.SetBackgroundColor(Color.ParseColor("#c7c793"));
+                        //txtView.SetBackgroundColor(Color.ParseColor("#c7c793"));
+                        txtView.SetBackgroundResource(Resource.Drawable.MyTextBackground6);
                         break;
                     case 6:
                         txtView.Text = txt7;
-                        txtView.SetBackgroundColor(Color.ParseColor("#c5718b"));
+                        //txtView.SetBackgroundColor(Color.ParseColor("#c5718b"));
+                        txtView.SetBackgroundResource(Resource.Drawable.MyTextBackground7);
                         break;
                     default:
                         txtView.Text = txt1;
-                        txtView.SetBackgroundColor(Color.ParseColor("#7dc3b7"));
+                        //txtView.SetBackgroundColor(Color.ParseColor("#7dc3b7"));
+                        txtView.SetBackgroundResource(Resource.Drawable.MyTextBackground1);
                         break;
                 }
             };
--- ProtoPyt01/ProtoPyt01/Resources/Resource.Designer.cs
+++ ProtoPyt01/ProtoPyt01/Resources/Resource.Designer.cs
@@ -54,13 +54,34 @@ namespace ProtoPyt01
 			public const int MyButtonBackground = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int Pyt1Button = 2130837507;
+			public const int MyTextBackground1 = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int WhiteRosePLastic = 2130837508;
+			public const int MyTextBackground2 = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int WhiteRosePLastic1 = 2130837509;
+			public const int MyTextBackground3 = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int MyTextBackground4 = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int MyTextBackground5 = 2130837511;
+			
+			// aapt resource value: 0x7f020008
+			public const int MyTextBackground6 = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int MyTextBackground7 = 2130837513;
+			
+			// aapt resource value: 0x7f02000a
+			public const int Pyt1Button = 2130837514;
+			
+			// aapt resource value: 0x7f02000b
+			public const int WhiteRosePLastic = 2130837515;
+			
+			// aapt resource value: 0x7f02000c
+			public const int WhiteRosePLastic1 = 2130837516;
 			
 			static Drawable()
 			{
