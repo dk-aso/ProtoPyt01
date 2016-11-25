8c4b4c30e3ce1dba36715c433d29d983c1fda0cf
alla3118 <alla3118@hedskole.dk>
2016-11-25 13:07:02 +0100

    2

--- ProtoPyt01/ProtoPyt01/Resources/Resource.Designer.cs
+++ ProtoPyt01/ProtoPyt01/Resources/Resource.Designer.cs
@@ -45,37 +45,40 @@ namespace ProtoPyt01
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int GiftIcon72 = 2130837504;
+			public const int design_button = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int Icon = 2130837505;
+			public const int GiftIcon72 = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int MyButtonBackground = 2130837506;
+			public const int Icon = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int MyTextBackground1 = 2130837507;
+			public const int MyButtonBackground = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int MyTextBackground2 = 2130837508;
+			public const int MyTextBackground1 = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int MyTextBackground3 = 2130837509;
+			public const int MyTextBackground2 = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int MyTextBackground4 = 2130837510;
+			public const int MyTextBackground3 = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int MyTextBackground5 = 2130837511;
+			public const int MyTextBackground4 = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int MyTextBackground6 = 2130837512;
+			public const int MyTextBackground5 = 2130837512;
 			
 			// aapt resource value: 0x7f020009
-			public const int MyTextBackground7 = 2130837513;
+			public const int MyTextBackground6 = 2130837513;
 			
 			// aapt resource value: 0x7f02000a
-			public const int Pyt1Button = 2130837514;
+			public const int MyTextBackground7 = 2130837514;
+			
+			// aapt resource value: 0x7f02000b
+			public const int Pyt1Button = 2130837515;
 			
 			static Drawable()
 			{
@@ -92,6 +95,9 @@ namespace ProtoPyt01
 			
 			// aapt resource value: 0x7f050000
 			public const int Pyt2Button = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int Pyt3Button = 2131034114;
 			
 			// aapt resource value: 0x7f050001
 			public const int TextView1 = 2131034113;
